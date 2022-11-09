5240e1fb49753b039b224dfcce54e4c5cd5b7a8e
Hossein Fadaei <mehrdadfadaee4@gmail.com>
2022-11-09 16:24:00 +0330

    start plugin

---
+++ plugin-filter.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: filter comment 
+Plugin URI: https://hosseindev.ir
+Description: This plugin is designed to filter swearing and inappropriate texts, and by activating it, your comments will be filtered
+Version: 1.0.0
+Requires PHP: 7.3
+Author: arashfadaee
+Author URI: https://hosseindev.ir
+
+*/
